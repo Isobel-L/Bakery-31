14d2b63d3ba2d928b93b868646915b481942c936
FKGregory <126529087+FKGregory@users.noreply.github.com>
2023-05-22 17:19:29 +0930

    Update Carrot.cpp
    added comments

--- src/Carrot.cpp
+++ src/Carrot.cpp
@@ -1,10 +1,12 @@
 #include "Carrot.h"
 
+//Carrot Constructor 
 Carrot::Carrot(int quantity) :
     Ingredient("Carrot", quantity)
 {
 }
 
+//Resturns price of a Carrot
 double Carrot::getPrice()
 {
     return 2.0;
